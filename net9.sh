cba548649a8b60553c8ed5fb64e901fd95b237b5
Ryan Davis <ryandavis.au@gmail.com>
2024-09-25 13:47:21 +1000

    simplify repro

--- net9.sh
+++ net9.sh
@@ -1,1 +1,1 @@
-rm -rf bin obj && dotnet restore InvalidProgram-net9.csproj && dotnet run --project InvalidProgram-net9.csproj -f net9.0-ios -c Release -p:ArchiveOnBuild=true -r:ios-arm64 /p:_DeviceName=$1 /p:EnableAssemblyILStripping=true -v:n
+rm -rf bin obj && dotnet restore InvalidProgram-net9.csproj && dotnet run --project InvalidProgram-net9.csproj -f net9.0-ios -c Release -p:ArchiveOnBuild=true -r:ios-arm64 /p:_DeviceName=$1 -v:n
